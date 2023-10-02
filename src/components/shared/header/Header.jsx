bfa2b78c0a0606be27195cd9cd861236372b948a
developertanvir2019 <developertanvir2019@gmail.com>
2023-10-03 01:28:40 +0600

    product card design done

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -5,7 +5,7 @@ import SecondNav from "./SecondNav";
 
 const Header = () => {
   return (
-    <div className="sticky -top-10 lg:-top-[6.6rem] md">
+    <div className="sticky -top-10 lg:-top-[6.6rem] md z-40">
       <div className="border-b border-slate-200 bg-primary">
         <div className="mx-auto grid w-full max-w-full grid-cols-4 gap-6 py-2 px-6 text-sm md:grid-cols-8 lg:max-w-5xl lg:grid-cols-12 xl:max-w-7xl 2xl:max-w-[96rem]">
           <div className="col-span-2 items-center md:col-span-4 lg:col-span-6">
