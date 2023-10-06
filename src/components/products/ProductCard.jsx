68c440a9ad218fef6d305d0da5fe5637a104d10a
developertanvir2019 <developertanvir2019@gmail.com>
2023-10-06 23:17:08 +0600

    design almost done

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,44 +1,68 @@
 import StarRatings from "react-star-ratings";
 import { FiEye, FiHeart, FiShoppingCart } from "react-icons/fi";
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
+import DetailsModal from "./DetailsModal";
 const ProductCard = () => {
   return (
-    <div className="w-60 group bg-gray-50 mx-4 my-4">
-      <div className="relative overflow-hidden">
-        <img
-          className="h-auto w-full object-cover"
-          src="https://themes.pixelstrap.com/bigdeal/assets/images/mega-store/product/7.jpg"
-          alt=""
-        />
-        <div className="flex justify-center bg-white">
-          <div>
-            <StarRatings
-              rating={2.403}
-              starDimension="15px"
-              starSpacing="3px"
-              starRatedColor="orange"
-            />
-            <h3 className="text-lg font-semibold text-gray-800">
-              Men fashion shoes
-            </h3>
-            <h3 className="text-lg font-semibold text-primary">
-              $50 <del className="text-gray-800">$70</del>
-            </h3>
+    <div>
+      <div className="w-60 group bg-gray-50 mx-4 my-4">
+        <div className="relative overflow-hidden">
+          <img
+            className="h-auto w-full object-cover"
+            src="https://themes.pixelstrap.com/bigdeal/assets/images/mega-store/product/7.jpg"
+            alt=""
+          />
+          <div className="flex justify-center bg-white">
+            <div>
+              <StarRatings
+                rating={2.403}
+                starDimension="15px"
+                starSpacing="3px"
+                starRatedColor="orange"
+              />
+              <h3 className="text-lg font-semibold text-gray-800">
+                Men fashion shoes
+              </h3>
+              <h3 className="text-lg font-semibold text-primary">
+                $50 <del className="text-gray-800">$70</del>
+              </h3>
+            </div>
           </div>
-        </div>
-        <div className="absolute h-full w-full bg-black/20 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
-          <div className="flex justify-center gap-3">
-            <div className="p-3 rounded-full bg-white text-lg hover:bg-primary hover:text-white">
-              <FiShoppingCart />
-            </div>
-            <div className="p-3 rounded-full bg-white text-lg hover:bg-primary hover:text-white">
-              <FiHeart />
-            </div>
-            <div className="p-3 rounded-full bg-white text-lg hover:bg-primary hover:text-white">
-              <FiEye />
+          <div className="absolute h-full w-full bg-black/20 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
+            <div className="flex justify-center gap-3">
+              <div
+                data-tooltip-id="add to cart"
+                data-tooltip-content="add to cart"
+                className="p-3 rounded-full bg-white text-lg hover:bg-primary hover:text-white"
+              >
+                <FiShoppingCart />
+                <Tooltip id="add to cart" />
+              </div>
+              <div
+                data-tooltip-id="add to wishlist"
+                data-tooltip-content="add to wishlist"
+                className="p-3 rounded-full bg-white text-lg hover:bg-primary hover:text-white"
+              >
+                <FiHeart />
+                <Tooltip id="add to wishlist" />
+              </div>
+              <div
+                onClick={() =>
+                  document.getElementById("detail_modal").showModal()
+                }
+                data-tooltip-id="view product"
+                data-tooltip-content="view product"
+                className="p-3 rounded-full bg-white text-lg hover:bg-primary hover:text-white"
+              >
+                <FiEye />
+                <Tooltip id="view product" />
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <DetailsModal />
     </div>
   );
 };
